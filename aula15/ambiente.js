d7f74e3aa7412faa8449758f64ff125c1aa8100d
Jader dos Santos <jaderwillians@gmail.com>
2022-12-19 11:36:03 -0300

    update

---
+++ aula15/ambiente.js
@@ -0,0 +1,11 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort() 
+num.length
+console.log(`Nosso vetor é o ${num}`)
+let pos = num.indexOf(9)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+console.log(`O valor 8 está na posição ${pos}`)
+}
